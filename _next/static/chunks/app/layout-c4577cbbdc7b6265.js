9f577d4f79405eedf367103e7b5a139a2b3f8cb1
Stan Cosmin <stancosminioan@gmail.com>
2025-06-06 13:33:17 +0300

    Updates

---
+++ _next/static/chunks/app/layout-c4577cbbdc7b6265.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{19324:()=>{},20835:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,40109,23)),Promise.resolve().then(l.t.bind(l,19324,23))},40109:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_4480e0"}}},e=>{var s=s=>e(e.s=s);e.O(0,[316,441,684,358],()=>s(20835)),_N_E=e.O()}]);
